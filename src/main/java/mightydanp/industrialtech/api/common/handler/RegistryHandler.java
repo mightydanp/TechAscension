5427c1593a78110e27f8b8c814ecafb8c29608fe
mightydanp <chrisdunbar12@gmail.com>
2020-09-27 05:28:20 -0500

    upload

---
+++ src/main/java/mightydanp/industrialtech/api/common/handler/RegistryHandler.java
@@ -0,0 +1,26 @@
+package mightydanp.industrialtech.api.common.handler;
+
+import mightydanp.industrialtech.api.common.libs.Ref;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+/**
+ * Created by MightyDanp on 9/26/2020.
+ */
+public class RegistryHandler {
+    protected static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Ref.mod_id);
+    protected static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Ref.mod_id);
+
+    public static DeferredRegister<Block>  getBlocks() {
+        return BLOCKS;
+    }
+
+    public static DeferredRegister<Item> getItems(){
+        return ITEMS;
+    }
+}
