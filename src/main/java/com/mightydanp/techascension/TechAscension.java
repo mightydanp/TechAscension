e59b8f28037f257f1713c9a2043b94cc2060483f
mightydanp <chrisdunbar12@gmail.com>
2024-03-16 02:55:15 -0500

    proof of concept

--- src/main/java/com/mightydanp/techascension/TechAscension.java
+++ src/main/java/com/mightydanp/techascension/TechAscension.java
@@ -37,7 +37,6 @@ public class TechAscension
     public TechAscension(IEventBus modEventBus) {
         modEventBus.addListener(this::commonSetup);
         NeoForge.EVENT_BUS.register(this);
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event){
