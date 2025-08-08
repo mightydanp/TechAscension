ee7ded060f5df1e3de914581c76a93dd4e10a61a
mightydanp <chrisdunbar12@gmail.com>
2025-08-08 05:34:19 -0500

    forge conversion

--- src/main/java/com/mightydanp/techascension/TechAscension.java
+++ src/main/java/com/mightydanp/techascension/TechAscension.java
@@ -1,45 +1,12 @@
 package com.mightydanp.techascension;
 
-import com.mightydanp.techascension.client.ref.ModRef;
-import com.mightydanp.techcore.client.ref.CoreRef;
-import com.mojang.logging.LogUtils;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
-import org.slf4j.Logger;
-@Mod(ModRef.MOD_ID)
-public class TechAscension
-{
-    private static final Logger LOGGER = LogUtils.getLogger();
+import net.minecraftforge.fml.common.Mod;
 
-    public TechAscension(IEventBus modEventBus) {
-        modEventBus.addListener(this::commonSetup);
-        NeoForge.EVENT_BUS.register(this);
+@Mod(TechAscension.MODID)
+public final class TechAscension {
+    public static final String MODID = "techascension";
+    public TechAscension() {
+        // Mod constructor logic
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event){
-        LOGGER.info("Tech Ascension common setup is starting");
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        LOGGER.info("Tech Ascension server is starting.");
-    }
-
-    @EventBusSubscriber(modid = CoreRef.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            LOGGER.info("Tech Ascension client setup is starting");
-        }
-    }
 }
--- src/main/java/com/mightydanp/techascension/client/ref/ModRef.java
+++
@@ -1,5 +0,0 @@
-package com.mightydanp.techascension.client.ref;
-
-public class ModRef {
-    public static final String MOD_ID = "techascension";
-}
