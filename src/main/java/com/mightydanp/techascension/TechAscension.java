2e7b05358f25ee2f0b4633fa731f12628357c776
mightydanp <chrisdunbar12@gmail.com>
2026-05-18 21:38:25 -0500

    -made me component ProcessingComponent. -made it where other mods on top of TechAscension can add their own components and add it between existing components. -added ore. Gave it processedStages to where when you processed ore it becomes centrifuged, purified, crushed. ore is just called ore. Mighty have to rethink how I name ore because of ore block and ore blockItem -you can now split ore -added any MaterialItem with quantity splittable

--- src/main/java/com/mightydanp/techascension/TechAscension.java
+++ src/main/java/com/mightydanp/techascension/TechAscension.java
@@ -3,9 +3,11 @@ package com.mightydanp.techascension;
 import com.mightydanp.techascension.client.ref.ModRef;
 import com.mightydanp.techascension.materials.TAMaterials;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.NewRegistryEvent;
 import org.slf4j.Logger;
 @Mod(ModRef.MOD_ID)
 public class TechAscension
@@ -15,6 +17,10 @@ public class TechAscension
     public TechAscension(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+        modEventBus.addListener(EventPriority.HIGHEST, this::bootstrapMaterials);
+    }
+
+    private void bootstrapMaterials(NewRegistryEvent event) {
         TAMaterials.commonInit();
     }
 }
